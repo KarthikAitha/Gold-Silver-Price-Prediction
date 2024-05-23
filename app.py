7a167ed685222eff342c14b0633a1911ebe2087d
KarthikAitha <97628537+KarthikAitha@users.noreply.github.com>
2024-05-23 11:52:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Load the model
-silver_model = load_model('models\silver_price_lstm_model.h5')
-gold_model = load_model('models\silver_price_lstm_model.h5')
+# Load the models
+silver_model = load_model('models/silver_price_lstm_model.h5')
+gold_model = load_model('models/gold_price_lstm_model.h5')
 
 # Load data from the database
 conn = sqlite3.connect('metal_prices.db')
@@ -22,10 +22,10 @@ data_gold = pd.read_sql(query_gold, conn)
 conn.close()
 
 # Convert the 'Date' column to datetime and set it as index
-data_silver['Date'] = pd.to_datetime(data_silver['Date'])
+data_silver['Date'] = pd.to_datetime(data_silver['Date'], dayfirst=True)
 data_silver.set_index('Date', inplace=True)
 
-data_gold['Date'] = pd.to_datetime(data_gold['Date'])
+data_gold['Date'] = pd.to_datetime(data_gold['Date'], dayfirst=True)
 data_gold.set_index('Date', inplace=True)
 
 # Select the relevant column (e.g., 'Close' price)
@@ -33,64 +33,56 @@ silver_prices = data_silver[['Close']]
 gold_prices = data_gold[['Close']]
 
 # Normalize the data
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(silver_prices)
-scaled_data = scaler.fit_transform(gold_prices)
+scaler_silver = MinMaxScaler(feature_range=(0, 1))
+scaled_data_silver = scaler_silver.fit_transform(silver_prices)
+
+scaler_gold = MinMaxScaler(feature_range=(0, 1))
+scaled_data_gold = scaler_gold.fit_transform(gold_prices)
 
 # Prepare the dataset for LSTM
 look_back = 60  # Use last 60 days to predict the next day
 
 # Predict the next five valid days' prices (skip weekends)
-next_days = 5
-last_days = scaled_data[-look_back:]
-silver_predictions = []
-gold_predictions = []
-future_dates = []
+def predict_next_days(model, scaled_data, look_back, scaler):
+    next_days = 5
+    last_days = scaled_data[-look_back:]
+    predictions = []
+    future_dates = []
 
-current_date = silver_prices.index[-1] + pd.Timedelta(days=1)
-while len(silver_predictions) < next_days:
-    if current_date.weekday() < 5:  # Monday to Friday are 0-4
-        last_days = np.reshape(last_days, (1, look_back, 1))
-        next_day_prediction = silver_model.predict(last_days)
-        silver_predictions.append(next_day_prediction[0][0])
-        future_dates.append(current_date)
-        next_day_prediction = np.reshape(next_day_prediction, (1, 1, 1))
-        last_days = np.append(last_days[:, 1:, :], next_day_prediction, axis=1)
-    current_date += pd.Timedelta(days=1)
+    current_date = silver_prices.index[-1] + pd.Timedelta(days=1)
+    while len(predictions) < next_days:
+        if current_date.weekday() < 5:  # Monday to Friday are 0-4
+            last_days = np.reshape(last_days, (1, look_back, 1))
+            next_day_prediction = model.predict(last_days)
+            predictions.append(next_day_prediction[0][0])
+            future_dates.append(current_date)
+            next_day_prediction = np.reshape(next_day_prediction, (1, 1, 1))
+            last_days = np.append(last_days[:, 1:, :], next_day_prediction, axis=1)
+        current_date += pd.Timedelta(days=1)
 
-# Invert the predictions
-silver_predictions = scaler.inverse_transform(np.array(silver_predictions).reshape(-1, 1))
+    # Invert the predictions
+    predictions = scaler.inverse_transform(np.array(predictions).reshape(-1, 1))
+    return predictions, future_dates
 
-current_date = gold_prices.index[-1] + pd.Timedelta(days=1)
-while len(gold_predictions) < next_days:
-    if current_date.weekday() < 5:  # Monday to Friday are 0-4
-        last_days = np.reshape(last_days, (1, look_back, 1))
-        next_day_prediction = silver_model.predict(last_days)
-        gold_predictions.append(next_day_prediction[0][0])
-        future_dates.append(current_date)
-        next_day_prediction = np.reshape(next_day_prediction, (1, 1, 1))
-        last_days = np.append(last_days[:, 1:, :], next_day_prediction, axis=1)
-    current_date += pd.Timedelta(days=1)
-
-# Invert the predictions
-gold_predictions = scaler.inverse_transform(np.array(gold_predictions).reshape(-1, 1))
+silver_predictions, future_dates_silver = predict_next_days(silver_model, scaled_data_silver, look_back, scaler_silver)
+gold_predictions, future_dates_gold = predict_next_days(gold_model, scaled_data_gold, look_back, scaler_gold)
 
 @app.route('/')
 def index():
     # Get the last 60 days prices for silver and gold
-    last_60_days_silver = scaler.inverse_transform(scaled_data[-look_back:])
+    last_60_days_silver = scaler_silver.inverse_transform(scaled_data_silver[-look_back:])
     last_60_days_silver_dates = silver_prices.index[-look_back:]
-    last_60_days_gold = scaler.inverse_transform(scaled_data[-look_back:])
+    last_60_days_gold = scaler_gold.inverse_transform(scaled_data_gold[-look_back:])
     last_60_days_gold_dates = gold_prices.index[-look_back:]
 
     # Combine future dates with their respective predictions for silver and gold
-    silver_predictions_with_dates = list(zip([date.strftime('%Y-%m-%d') for date in future_dates], silver_predictions.flatten()))
-    gold_predictions_with_dates = list(zip([date.strftime('%Y-%m-%d') for date in future_dates], gold_predictions.flatten()))
+    silver_predictions_with_dates = list(zip([date.strftime('%Y-%m-%d') for date in future_dates_silver], silver_predictions.flatten()))
+    gold_predictions_with_dates = list(zip([date.strftime('%Y-%m-%d') for date in future_dates_gold], gold_predictions.flatten()))
 
     # Plot the chart for silver
     fig_silver = go.Figure()
     fig_silver.add_trace(go.Scatter(x=last_60_days_silver_dates, y=last_60_days_silver.flatten(), mode='lines', name='Last 60 Days Prices'))
-    fig_silver.add_trace(go.Scatter(x=future_dates, y=silver_predictions.flatten(), mode='lines', name='Next 5 Days Predictions'))
+    fig_silver.add_trace(go.Scatter(x=future_dates_silver, y=silver_predictions.flatten(), mode='lines', name='Next 5 Days Predictions'))
     fig_silver.update_layout(
         title='Silver Price Prediction',
         xaxis_title='Date',
@@ -102,7 +94,7 @@ def index():
     # Plot the chart for gold
     fig_gold = go.Figure()
     fig_gold.add_trace(go.Scatter(x=last_60_days_gold_dates, y=last_60_days_gold.flatten(), mode='lines', name='Last 60 Days Prices'))
-    fig_gold.add_trace(go.Scatter(x=future_dates, y=gold_predictions.flatten(), mode='lines', name='Next 5 Days Predictions'))
+    fig_gold.add_trace(go.Scatter(x=future_dates_gold, y=gold_predictions.flatten(), mode='lines', name='Next 5 Days Predictions'))
     fig_gold.update_layout(
         title='Gold Price Prediction',
         xaxis_title='Date',
@@ -112,7 +104,6 @@ def index():
     plot_html_gold = pio.to_html(fig_gold, full_html=False)
 
     return render_template('index.html', plot_html_silver=plot_html_silver, plot_html_gold=plot_html_gold, silver_predictions=silver_predictions_with_dates, gold_predictions=gold_predictions_with_dates)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
